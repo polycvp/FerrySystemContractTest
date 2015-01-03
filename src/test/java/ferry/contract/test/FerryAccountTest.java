597ab807f23f5e20f6d72537403dc8555a0c97a2
polycvp <poly_cvp@yahoo.com>
2015-01-03 19:52:06 +0100

    initial testing setup

---
+++ src/test/java/ferry/contract/test/FerryAccountTest.java
@@ -0,0 +1,39 @@
+package ferry.contract.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FerryAccountTest {
+    
+    public FerryAccountTest() {
+    }
+
+     @Test
+     public void testMakeAccount() {
+         System.out.println("!");
+     }
+      @Test
+     public void testEditAccount() {
+         System.out.println("!");
+     }
+       @Test
+     public void testDeleteAccount() {
+         System.out.println("!");
+     }
+       @Test
+     public void testShowAccount() {
+         System.out.println("!");
+     }
+       @Test
+     public void testValidLogin() {
+         System.out.println("!");
+     }
+       @Test
+     public void testInvalidLogin() {
+         System.out.println("!");
+     }
+       @Test
+     public void testIsUserResident() {
+         System.out.println("!");
+     }
+}
---
+++ src/test/java/ferry/contract/test/FerryReservationTest.java
@@ -0,0 +1,34 @@
+package ferry.contract.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Paul
+ */
+public class FerryReservationTest {
+
+    public FerryReservationTest() {
+    }
+
+    @Test
+    public void testMakeReservation() {
+        System.out.println("!");
+    }
+
+    @Test
+    public void testEditReservation() {
+        System.out.println("!");
+    }
+
+    @Test
+    public void testDeleteReservation() {
+        System.out.println("!");
+    }
+    
+    @Test
+    public void testShowReservation() {
+        System.out.println("!");
+    }
+}
