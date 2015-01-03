8eb296a189351502023e22ef4cd9c8984a7cb6bc
polycvp <poly_cvp@yahoo.com>
2015-01-03 21:27:15 +0100

    added first draft for tests

--- src/test/java/ferry/contract/test/FerryAccountTest.java
+++ src/test/java/ferry/contract/test/FerryAccountTest.java
@@ -1,39 +1,52 @@
 package ferry.contract.test;
 
+import static ferry.contract.test.FerryContractHolder.contract;
+import ferry.dto.AbstractAccount;
+import ferry.dto.AccountDetail;
+import ferry.dto.AccountSummary;
+import ferry.eto.InvalidAccountException;
+import ferry.eto.NoSuchAccountException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FerryAccountTest {
-    
+    private AccountDetail ad = new AccountDetail(12, "1234561234", "password", "Test1", "test@test.dk", "DK");
     public FerryAccountTest() {
     }
 
      @Test
-     public void testMakeAccount() {
-         System.out.println("!");
+     public void testMakeAccount() throws InvalidAccountException {
+        boolean makeAccount = contract.makeAccount(ad);
+        assertTrue(makeAccount);
      }
       @Test
-     public void testEditAccount() {
-         System.out.println("!");
+     public void testEditAccount() throws NoSuchAccountException {
+         ad.setAddress("EN");
+        boolean editAccount = contract.editAccount(ad);
+        assertTrue(editAccount);
+     }
+        @Test
+     public void testShowAccount() throws NoSuchAccountException {
+        AccountDetail showAccount = contract.showAccount(new AbstractAccount(1) {});
+        assertEquals(showAccount.getId(),1);
      }
        @Test
-     public void testDeleteAccount() {
-         System.out.println("!");
+     public void testDeleteAccount() throws NoSuchAccountException {
+        AccountSummary deleteAccount = contract.deleteAccount(ad);
+        assertEquals(deleteAccount.getId(),ad.getId());
      }
        @Test
-     public void testShowAccount() {
-         System.out.println("!");
+     public void testValidLogin() throws NoSuchAccountException {
+        AccountDetail login = contract.login("user@user.dk", "password");
+        assertEquals(1,login.getId());
      }
-       @Test
-     public void testValidLogin() {
-         System.out.println("!");
-     }
-       @Test
-     public void testInvalidLogin() {
-         System.out.println("!");
+       @Test(expected=NoSuchAccountException.class)
+     public void testInvalidLogin() throws NoSuchAccountException {
+         AccountDetail login = contract.login("user@user.dk", "ionvalid");
      }
        @Test
      public void testIsUserResident() {
-         System.out.println("!");
+        boolean userResident = contract.isUserResident(new AbstractAccount(1) {});
+        assertTrue(userResident);
      }
 }
--- src/test/java/ferry/contract/test/FerryReservationTest.java
+++ src/test/java/ferry/contract/test/FerryReservationTest.java
@@ -1,34 +1,46 @@
 package ferry.contract.test;
 
+import static ferry.contract.test.FerryContractHolder.contract;
+import ferry.dto.AbstractVehicle;
+import ferry.dto.AccountDetail;
+import ferry.dto.AccountSummary;
+import ferry.dto.PassengerDTO;
+import ferry.dto.ReservationDetail;
+import ferry.dto.ReservationSummary;
+import ferry.eto.NoSuchAccountException;
+import ferry.eto.NoSuchReservationException;
+import java.util.ArrayList;
+import java.util.Date;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Paul
- */
+
 public class FerryReservationTest {
 
     public FerryReservationTest() {
     }
 
     @Test
-    public void testMakeReservation() {
-        System.out.println("!");
+    public void testMakeReservation() throws NoSuchReservationException {
+        AccountSummary accS = new AccountSummary(12,"1234561234", "Test3", "test3@test.dk", "EU");
+        ArrayList<PassengerDTO> passengers = new ArrayList<PassengerDTO>();
+        ArrayList<AbstractVehicle> vehicles = new ArrayList<AbstractVehicle>();
+        
+        ReservationDetail resDetail = new ReservationDetail(accS, passengers, vehicles, "Constanta", "Copenhagen", 
+               new Date(), new Date(), 4590, "15RES31");
+        ReservationSummary makeReservation = contract.makeReservation(resDetail);
+        assertTrue(makeReservation.getReservationSerialNumber()=="15RES31");
+    }
+
+    @Test(expected =UnsupportedOperationException.class)
+    public void testEditReservation() throws Exception {
+        boolean editAccount = contract.editAccount(new AccountDetail(123, "1234561234", "password", "test100", "test100@test.dk", "ES"));
+        assertTrue(editAccount);
     }
 
     @Test
-    public void testEditReservation() {
-        System.out.println("!");
-    }
-
-    @Test
-    public void testDeleteReservation() {
-        System.out.println("!");
+    public void testDeleteReservation() throws NoSuchReservationException {
+        assertTrue(contract.deleteReservation(1));
     }
     
-    @Test
-    public void testShowReservation() {
-        System.out.println("!");
-    }
 }
